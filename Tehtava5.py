f51988fc9e52c1d93f0976342631aca4ddadbb04
Lenni Marjomaa <lennima@metropolia.fi>
2022-08-26 13:07:58 +0300

    Initial commit

---
+++ Tehtava5.py
@@ -0,0 +1,10 @@
+Luoti=13.3
+Naula=32*Luoti
+Leiviska=20*Naula
+
+leiviskat=input("Kuinka_monta_Leiviskaa:")
+leiviskatGrammoina=float(leiviskat)*Leiviska
+print(leiviskatGrammoina)
+
+naulat=input("Kuinka monta_Naulaa:")
+naulatGrammoina=float(Naula*)
